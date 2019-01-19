ccca6e46900fa69e815642774252df83bfbb638c
Donald Lee <donaldleeqs@gmail.com>
2019-01-19 20:23:20 +0800

    fix for popup not showing

--- popup.js
+++ popup.js
@@ -4,15 +4,16 @@ $('.dropdown-menu a').click(function () {
   $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
 });
 
+const credentials = { login : 'matthea1998', password: 'password' };
+
 function getDuoLingoData() {
   fetch(url, {
     method: 'post',
     headers: {
       "Content-type": "application/json"
     },
-    body: 'foo=bar&lorem=ipsum'
-  })
-    .then(json)
+    body: JSON.stringify(credentials)
+  }).then(json)
     .then(function (data) {
       console.log('Request succeeded with JSON response', data);
     })
@@ -21,18 +22,12 @@ function getDuoLingoData() {
     });
 }
 
-$('.dropdown-menu a').click(function() {
-	$(this).parents('.dropdown').find('.btn').html($(this).text());
-	$(this).parents('.dropdown').find('.btn').val($(this).data('value'));
+$('.dropdown-menu a').click(function () {
+  $(this).parents('.dropdown').find('.btn').html($(this).text());
+  $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
 });
 
-<<<<<<< HEAD
-$('#submitBtn').click(function() {
-	console.log('Login');
+$('#submitBtn').click(function (event) {
+  event.preventDefault();
+  getDuoLingoData();
 });
-=======
-$('#submitBtn').click(function(event) {
-	 event.preventDefault();
-	 console.log('login');
-});
->>>>>>> d7904bb63660acc6b9afe80af203665949c5b3de
