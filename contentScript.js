73bb4f683fa0ab10feb3c70287e6ea9e0901482e
Donald Lee <donaldleeqs@gmail.com>
2019-01-20 02:28:54 +0800

    add fast replacement

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,56 @@
+const NUM_REPLACE = 25;
+
 function replaceForLanguage(name) {
   const url = chrome.runtime.getURL(`corpus/${name}.json`);
 
   fetch(url)
     .then((response) => response.json()) //assuming file contains json
     .then((json) => {
-      for (let key in json) {
-        if (json.hasOwnProperty(key)) {
-          console.log(`Replacing ${key} with ${json[key]}`);
-          let re = '\\s+' + key;
-          let val = ' ' + json[key];
+      let body = document.body.innerText.trim().toLowerCase().split(/\s+/);
+      body = body.map(elem => elem.toLowerCase().trim());
+      //let counts = {};
+
+      //for (let i = 0; i < body.length; i++) {
+      //  let num = body[i];
+      //  counts[num] = counts[num] ? counts[num] + 1 : 1;
+      //}
+      //const keysSorted = Object.keys(counts).sort(function(a,b){return counts[a]-counts[b]})
+      //console.log(keysSorted);
+
+      let numReplaced = 0;
+      
+      const keysSorted = body;
+      // find 
+      for (let word of keysSorted) {
+        if (json.hasOwnProperty(word)) {
+          numReplaced++;
+          console.log(`Replacing ${word} with ${json[word]}`);
+          let re = '\\s+' + word + '\\s+';
+          let val = ' <ins>' + json[word] + '</ins> ';
           document.body.innerHTML = document.body.innerHTML.replace(new RegExp(re, "gi"), val);
+          if (numReplaced > NUM_REPLACE) {
+            return;
+          }
         }
       }
     })
 }
 
-replaceForLanguage('test');
+function foo(arr) {
+  var a = [], b = [], prev;
+
+  arr.sort();
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] !== prev) {
+      a.push(arr[i]);
+      b.push(1);
+    } else {
+      b[b.length - 1]++;
+    }
+    prev = arr[i];
+  }
+
+  return [a, b];
+}
+
+replaceForLanguage('Corpus-fr');
--- popup.js
+++ popup.js
@@ -4,24 +4,6 @@ $('.dropdown-menu a').click(function () {
   $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
 });
 
-const credentials = { login : 'matthea1998', password: 'password' };
-
-function getDuoLingoData() {
-  fetch(url, {
-    method: 'post',
-    headers: {
-      "Content-type": "application/json"
-    },
-    body: JSON.stringify(credentials)
-  }).then(json)
-    .then(function (data) {
-      console.log('Request succeeded with JSON response', data);
-    })
-    .catch(function (error) {
-      console.log('Request failed', error);
-    });
-}
-
 $('.dropdown-menu a').click(function () {
   $(this).parents('.dropdown').find('.btn').html($(this).text());
   $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
@@ -34,5 +16,4 @@ $('#loginForm').submit(function(event) {
   let email = $('input').eq(0).val();	 
   let password = $('input').eq(1).val();
 
-  getDuoLingoData();
 });
