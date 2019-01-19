4ebbf1c235eb7d9987736670932503e29ea865f6
mmdlow <mmdlow@NEW-DG0GMQB27F1.localdomain>
2019-01-19 18:50:14 +0800

    Create basic login

--- popup.js
+++ popup.js
@@ -1,20 +1,9 @@
-let changeColor = document.getElementById('changeColor');
-
-// chrome.storage.sync.get('color', function (data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-// changeColor.onclick = function (element) {
-//   let color = element.target.value;
-//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     chrome.tabs.executeScript(
-//       tabs[0].id,
-//       { code: 'document.body.style.backgroundColor = "' + color + '";' });
-//   });
-// };
-
 // Change Dropdown menu title to option text
 $('.dropdown-menu a').click(function() {
 	$(this).parents('.dropdown').find('.btn').html($(this).text());
 	$(this).parents('.dropdown').find('.btn').val($(this).data('value'));
+});
+
+$('#submitBtn').click(function() {
+	console.log('Login');
 });
