3417f2f70305ea2f91257cdbb2ba2a1d42d9c713
mmdlow <mmdlow@NEW-DG0GMQB27F1.localdomain>
2019-01-19 22:14:00 +0800

    Enhance CSS

--- popup.js
+++ popup.js
@@ -4,6 +4,20 @@ $('.dropdown-menu a').click(function () {
   $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
 });
 
+// Handle Dropdown search for languages
+$('#langSearch').keyup(function() {
+  const filter = $('#langSearch').eq(0).val().toUpperCase();
+  const a = $('#dropdownMenu a');
+  for (let i = 0; i < a.length; i++) {
+    let txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+});
+
 const credentials = { login : 'matthea1998', password: 'password' };
 
 function getDuoLingoData() {
@@ -21,11 +35,6 @@ function getDuoLingoData() {
       console.log('Request failed', error);
     });
 }
-
-$('.dropdown-menu a').click(function () {
-  $(this).parents('.dropdown').find('.btn').html($(this).text());
-  $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
-});
 
 // Handle login
 $('#loginForm').submit(function(event) {
