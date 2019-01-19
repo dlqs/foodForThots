92c05cc01b961201f297b3d5ca8250f4ad8a5e9d
Donald Lee <donaldleeqs@gmail.com>
2019-01-19 16:58:26 +0800

    add support for multiple corpus fies

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,16 @@
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("integrated", "gi"), "FUCK");
+function replaceForLanguage(name) {
+  const url = chrome.runtime.getURL(`corpus/${name}.json`);
+
+  fetch(url)
+    .then((response) => response.json()) //assuming file contains json
+    .then((json) => {
+      for (let key in json) {
+        if (json.hasOwnProperty(key)) {
+          console.log(`Replacing ${key} with ${json[key]}`);
+          document.body.innerHTML = document.body.innerHTML.replace(new RegExp(key, "gi"), json[key]);
+        }
+      }
+    })
+}
+
+replaceForLanguage('test');
