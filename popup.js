258a40346b164d1200ed18b079f0a2c10ed9bf72
Donald Lee <donaldleeqs@gmail.com>
2019-01-20 09:24:59 +0800

    move function over to popup

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+
 // Change Dropdown menu title to option text
 $('.dropdown-menu a').click(function () {
   $(this).parents('.dropdown').find('.btn').html($(this).text());
@@ -36,4 +37,37 @@ $('#loginForm').submit(function(event) {
 $('#startGameForm').submit(function(event) {
   event.preventDefault();
   const selectedLang = $('#dropdownMenuButton').text();
+  switch (selectedLang) {
+    case ''
+  }
 });
+
+const NUM_REPLACE = 25;
+
+function replaceForLanguage(name) {
+  const url = chrome.runtime.getURL(`corpus/${name}.json`);
+
+  fetch(url)
+    .then((response) => response.json()) //assuming file contains json
+    .then((json) => {
+      let body = document.body.innerText.trim().toLowerCase().split(/\s+/);
+      body = body.map(elem => elem.toLowerCase().trim());
+
+      let numReplaced = 0;
+      
+      for (let word of body) {
+        if (json.hasOwnProperty(word)) {
+          numReplaced++;
+          console.log(`Replacing ${word} with ${json[word]}`);
+          let re = '\\s+' + word + '\\s+';
+          let val = ' <ins>' + json[word] + '</ins> ';
+          document.body.innerHTML = document.body.innerHTML.replace(new RegExp(re, "gi"), val);
+          if (numReplaced > NUM_REPLACE) {
+            return;
+          }
+        }
+      }
+    })
+}
+
+replaceForLanguage('Corpus-fr');
